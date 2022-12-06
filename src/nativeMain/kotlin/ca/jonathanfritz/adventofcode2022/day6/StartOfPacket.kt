f54edf796f6be3eac39bf1095dd215d198c5797e
Jonathan <jonfritz@gmail.com>
2022-12-06 07:10:48 -0500

    Day 6: Part 2

--- src/nativeMain/kotlin/ca/jonathanfritz/adventofcode2022/day6/StartOfPacket.kt
+++ src/nativeMain/kotlin/ca/jonathanfritz/adventofcode2022/day6/StartOfPacket.kt
@@ -2,11 +2,15 @@ package ca.jonathanfritz.adventofcode2022.day6
 
 class StartOfPacket {
 
-    fun part1 (datastream: String): Int {
-        (0 .. datastream.length - 4).forEach { offset ->
-            val substring = datastream.substring(offset, offset+4)
+    fun part1 (datastream: String) = findUniqueSubstringOffset(datastream, 4)
+
+    fun part2 (datastream: String) = findUniqueSubstringOffset(datastream, 14)
+
+    private fun findUniqueSubstringOffset(datastream: String, substringLength: Int): Int {
+        (0..datastream.length - substringLength).forEach { offset ->
+            val substring = datastream.substring(offset, offset + substringLength)
             if (substring.toCharArray().none { char -> substring.toCharArray().count { it == char } > 1 })
-            return offset+4
+                return offset + substringLength
         }
         return -1
     }
--- src/nativeTest/kotlin/ca/jonathanfritz/adventofcode2022/day6/StartOfPacketTest.kt
+++ src/nativeTest/kotlin/ca/jonathanfritz/adventofcode2022/day6/StartOfPacketTest.kt
@@ -10,33 +10,64 @@ internal class StartOfPacketTest {
     private val startOfPacket = StartOfPacket()
 
     @Test
-    fun offsetTest() {
+    fun startOfPacketTest() {
         assertEquals(7, startOfPacket.part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
     }
 
     @Test
-    fun offsetTest2() {
+    fun startOfPacketTest2() {
         assertEquals(5, startOfPacket.part1("bvwbjplbgvbhsrlpgdmjqwftvncz"))
     }
 
     @Test
-    fun offsetTest3() {
+    fun startOfPacketTest3() {
         assertEquals(6, startOfPacket.part1("nppdvjthqldpwncqszvftbrmjlhg"))
     }
 
     @Test
-    fun offsetTest4() {
+    fun startOfPacketTest4() {
         assertEquals(10, startOfPacket.part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
     }
 
     @Test
-    fun offsetTest5() {
+    fun startOfPacketTest5() {
         assertEquals(11, startOfPacket.part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
     }
 
     @Test
-    fun offset() {
+    fun startOfPacket() {
         val line = readResourcesFile("day6/datastream.txt")[0]
         assertEquals(1287, startOfPacket.part1(line))
+    }
+
+    @Test
+    fun startOfMessageTest() {
+        assertEquals(19, startOfPacket.part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+    }
+
+    @Test
+    fun startOfMessageTest2() {
+        assertEquals(23, startOfPacket.part2("bvwbjplbgvbhsrlpgdmjqwftvncz"))
+    }
+
+    @Test
+    fun startOfMessageTest3() {
+        assertEquals(23, startOfPacket.part2("nppdvjthqldpwncqszvftbrmjlhg"))
+    }
+
+    @Test
+    fun startOfMessageTest4() {
+        assertEquals(29, startOfPacket.part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+    }
+
+    @Test
+    fun startOfMessageTest5() {
+        assertEquals(26, startOfPacket.part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+    }
+
+    @Test
+    fun startOfMessage() {
+        val line = readResourcesFile("day6/datastream.txt")[0]
+        assertEquals(3716, startOfPacket.part2(line))
     }
 }
